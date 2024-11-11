f6ab3724724bd019eaa08c9d9b3f317cd09ec63f
nghiadoan2310 <nghiadoan23102002@gmail.com>
2024-11-11 18:21:25 +0700

    button, input

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState<number>(0)
+  const [name, setName] = useState<string>("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Name: {name}</Text>
+      <TextInput style={styles.input} onChangeText={(value) => setName(value)} placeholder='Enter your name' maxLength={20} autoCorrect={false}/>
+      <Text style={styles.text}>count: {count}</Text>
+      <Button title='increase' onPress={() => setCount(count + 1)}/>
     </View>
   );
 }
@@ -17,4 +23,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    width: 200,
+    padding: 10,
+    borderColor: '#737373',
+    borderWidth: 1,
+    marginBottom: 16,
+    textDecorationLine: 'none',
+
+  }
 });
