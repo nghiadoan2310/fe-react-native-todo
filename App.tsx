1e7208dc79b6c2ce3ec552019388154750e99730
nghiadoan2310 <nghiadoan23102002@gmail.com>
2024-11-12 12:04:29 +0700

    to do app

--- App.tsx
+++ App.tsx
@@ -1,70 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, FlatList, Keyboard, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
+
+interface Itodo {
+  id: number;
+  text: string;
+}
 
 export default function App() {
-  const FOODLIST = [
-    'Apple',
-    'Banana',
-    'Pear',
-    'Orange',
-    'Grapes',
-    'Strawberry',
-    'Mango',
-    'Pineapple',
-    'Watermelon',
-    'Papaya',
-    'Cherry',
-    'Coconut',
-    'Milk',
-    'Rice',
-    'Sugar',
-    'Bread',
-    'Cheese',
-    'Tomato',
-    'Cucumber',
-    'Lettuce',
-    'Broccoli',
-    'Carrot',
-    'Corn',
-    'Onion',
-    'Garlic',
-    'Peas',
-    'Zucchini',
-    'Eggplant',
-    'Radish',
-    'Spinach',
-    'Broccoli',
-    'Bell Pepper',
-    'Cabbage',
-    'Sweet Potato',
-    'Corn',
-    'Kale',
-  ]
+  const [itemValue, setItemValue] = useState("")
+  const [itemArray, setItemArray] = useState<Itodo[]>([])
+
+  const handleAddItem = () => {
+    if (itemValue) {
+      setItemArray([...itemArray, { 
+        id: itemArray.length + 1,
+        text: itemValue,
+      }])
+      setItemValue("")
+    } else {
+      Alert.alert('Lỗi', 'input đang trống', [
+        {text: 'xác nhận' },
+      ])
+    }
+  }
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={FOODLIST}
-        renderItem={({ item }) => (
-          <View 
-            style={{
-              backgroundColor: '#f2f2f2',
-              padding: 16,
-              borderRadius: 8,
-              marginBottom: 20,
-              width: '90%',
-              alignSelf: 'center',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <Text style={styles.text}>{item}</Text>
-          </View>
-        )}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View>
+        <Text style={styles.title}>To Do App</Text>
+        <View>
+          <TextInput 
+            style={styles.input} 
+            placeholder="Add a new item" 
+            onChangeText={text => setItemValue(text)} 
+            value={itemValue}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button title="Add Item" onPress={handleAddItem} />
+        </View>
+        <FlatList
+          data={itemArray}
+          renderItem={({ item }) => (
+            <View 
+              style={{
+                backgroundColor: '#f2f2f2',
+                padding: 16,
+                borderRadius: 8,
+                marginBottom: 20,
+                width: '90%',
+                alignSelf: 'center',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <Text style={styles.text}>{item.text}</Text>
+            </View>
+          )}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -72,8 +69,34 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff'
   },
+  title:{
+    color: '#333',
+    textAlign: 'center',
+    marginVertical: 25,
+    fontSize: 40,
+    backgroundColor: 'rgba(255, 255, 0, 0.4)',
+    paddingVertical: 10
+  },
   text: {
     fontSize: 16,
     fontWeight: 'bold',
   },
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    fontSize: 18,
+    backgroundColor: '#f2f2f2',
+    width: '90%',
+    alignSelf: 'center',
+    paddingVertical: 10,
+  },
+  button: {
+    width: '50%',
+    alignSelf: 'center',
+    marginBottom: 20,
+    paddingVertical: 10,
+  }
 });
